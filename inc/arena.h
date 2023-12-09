55ee65627c9c8d3fc07511cf715992480197f3f0
wdhg <me@wdhg.me>
2023-12-09 15:29:58 +0000

    Update macros

--- inc/arena.h
+++ inc/arena.h
@@ -10,13 +10,13 @@ void ArenaFree(Arena *arena);
 void *ArenaPush(Arena *arena, size_t size);
 void *ArenaPushZero(Arena *arena, size_t size);
 
-#define arena_push_array(arena, count, type)                                   \
-	(type *)arena_push((arena), (count) * sizeof(type))
-#define arena_push_array_zero(arena, count, type)                              \
-	(type *)arena_push_zero((arena), (count) * sizeof(type))
+#define ArenaPushArray(arena, count, type)                                     \
+	(type *)ArenaPush((arena), (count) * sizeof(type))
+#define ArenaPushArrayZero(arena, count, type)                                 \
+	(type *)ArenaPushZero((arena), (count) * sizeof(type))
 
-#define arena_push_struct(arena, type) (type *)arena_push((arena), sizeof(type))
-#define arena_push_struct_zero(arena, type)                                    \
-	(type *)arena_push_zero((arena), sizeof(type))
+#define ArenaPushStruct(arena, type) (type *)ArenaPush((arena), sizeof(type))
+#define ArenaPushStructZero(arena, type)                                       \
+	(type *)ArenaPushZero((arena), sizeof(type))
 
 #endif
